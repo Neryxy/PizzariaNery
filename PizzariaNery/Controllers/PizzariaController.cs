9b1e86995093ac012912a97a57be900e63bbb987
Vitor <vitormortagonery@gmail.com>
2026-04-29 16:28:03 -0300

    Feat: Criando a Controller Pizzaria

--- PizzariaNery/Controllers/PizzariaController.cs
+++ PizzariaNery/Controllers/PizzariaController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pizzaria.Models;
 
-namespace PizzariaNery.Controllers
+namespace Pizzaria.Controllers
 {
     public class PizzariaController : Controller
     {
-        public IActionResult Index()
+        public IActionResult NovoPedido()
         {
             return View();
         }
+        // pedido é uma instancia da classe Pedido, que é preenchida com os dados do formulário enviado pelo usuário
+        public IActionResult NotaPedido(Pedido pedido)
+        {
+            ViewBag.total = pedido.CalcularPedido();
+            pedido.horarioPedido = DateTime.Now;
+            return View(pedido);
+        }
+        // o viewbag é um objeto dinâmico que permite mostrar dados internos na view
     }
 }
